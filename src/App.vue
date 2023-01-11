0b3723c4a235f6e53f9a605aadd8d109fd9e50a0
Briantam0422 <briantam0422@gmail.com>
2023-01-11 10:30:23 -0800

    create login page

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,6 @@
-<script setup>
-// import { RouterLink, RouterView } from "vue-router";
-// import HelloWorld from "./components/HelloWorld.vue";
-// import LoginView from "./views/LoginView.vue";
-</script>
+<script setup></script>
 
 <template>
-<!--  <header>-->
-<!--    &lt;!&ndash;    <img&ndash;&gt;-->
-<!--    &lt;!&ndash;      alt="Vue logo"&ndash;&gt;-->
-<!--    &lt;!&ndash;      class="logo"&ndash;&gt;-->
-<!--    &lt;!&ndash;      src="@/assets/logo.svg"&ndash;&gt;-->
-<!--    &lt;!&ndash;      width="125"&ndash;&gt;-->
-<!--    &lt;!&ndash;      height="125"&ndash;&gt;-->
-<!--    &lt;!&ndash;    />&ndash;&gt;-->
-<!--    <div class="wrapper">-->
-<!--      <LoginView />-->
-<!--      &lt;!&ndash;      <nav>&ndash;&gt;-->
-<!--      &lt;!&ndash;        <RouterLink to="/">Home</RouterLink>&ndash;&gt;-->
-<!--      &lt;!&ndash;        <RouterLink to="/about">About</RouterLink>&ndash;&gt;-->
-<!--      &lt;!&ndash;      </nav>&ndash;&gt;-->
-<!--    </div>-->
-<!--  </header>-->
-
   <RouterView />
 </template>
 
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,42 +1,25 @@
 <script setup></script>
 
 <template>
-  <div>Login
-  <button class="btn btn-primary">fdsaf</button>
-    <div class="accordion" id="accordionExample">
-      <div class="accordion-item">
-        <h2 class="accordion-header" id="headingOne">
-          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            Accordion Item #1
-          </button>
-        </h2>
-        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-          <div class="accordion-body">
-            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-          </div>
-        </div>
-      </div>
-      <div class="accordion-item">
-        <h2 class="accordion-header" id="headingTwo">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            Accordion Item #2
-          </button>
-        </h2>
-        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-          <div class="accordion-body">
-            <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-          </div>
-        </div>
-      </div>
-      <div class="accordion-item">
-        <h2 class="accordion-header" id="headingThree">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-            Accordion Item #3
-          </button>
-        </h2>
-        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-          <div class="accordion-body">
-            <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="d-flex justify-content-center align-items-center">
+          <div class="card shadow">
+            <div class="card-body">
+              <h1>Login</h1>
+              <div class="form mb-5 mt-5">
+                <div class="mb-3">
+                  <label class="form-label" for="username"> Username </label>
+                  <input id="username" name="username" class="form-control" />
+                </div>
+                <div>
+                  <label class="form-label" for="password"> Password </label>
+                  <input id="password" name="password" class="form-control" />
+                </div>
+              </div>
+              <button id="login" class="w-100 btn btn-primary">Login</button>
+            </div>
           </div>
         </div>
       </div>
@@ -44,4 +27,13 @@
   </div>
 </template>
 
-<style></style>
+<style>
+.container .card {
+  min-width: 500px;
+  max-width: 500px;
+}
+
+.container .card .card-body {
+  padding: 50px;
+}
+</style>
