03a7419a3e836c3f0056472303f3ff046c7abc21
Briantam0422 <briantam0422@gmail.com>
2023-01-11 22:41:27 -0800

    - update login ui page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,7 +7,14 @@
         <div class="d-flex justify-content-center align-items-center">
           <div class="card shadow">
             <div class="card-body">
-              <h1>Login</h1>
+              <div class="text-center">
+                <img
+                  class="app-logo"
+                  src="../assets/logo/chatapp_logo.png"
+                  alt="chatapp_logo"
+                />
+                <p class="text-black-50">A Fast and Real Time Chat App</p>
+              </div>
               <div class="form mb-5 mt-5">
                 <div class="mb-3">
                   <label class="form-label" for="username"> Username </label>
@@ -18,7 +25,15 @@
                   <input id="password" name="password" class="form-control" />
                 </div>
               </div>
-              <button id="login" class="w-100 btn btn-primary">Login</button>
+              <button id="login" class="w-100 btn btn-primary mb-2">Login</button>
+              <button class="w-100 text-center btn btn-link">
+                Don't have an Account? Create One Now
+              </button>
+              <button
+                class="w-100 btn btn-link text-black-50 text-center tx-12"
+              >
+                Terms and Privacy
+              </button>
             </div>
           </div>
         </div>
@@ -35,5 +50,10 @@
 
 .container .card .card-body {
   padding: 50px;
+}
+
+.app-logo {
+  width: 100%;
+  max-width: 150px;
 }
 </style>
